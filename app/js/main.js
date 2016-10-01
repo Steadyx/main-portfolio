79abd149a81a61e5e9d68118a14d43ee86bba332
Edward Kemp <edwardlanekemp@gmail.com>
2016-10-01 11:34:10 +0100

    Making responsive changes to about section, added media queries and parallax

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,27 @@
-$('.dropdown-button').dropdown({
-  inDuration: 300,
-  outDuration: 225,
-  hover: true, // Activate on hover
-  gutter: 0, // Spacing from edge
-  belowOrigin: true, // Displays dropdown below the button
-  alignment: 'right' // Displays dropdown with edge aligned to the left of button
-});
+(function($) {
+
+  $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'right' // Displays dropdown with edge aligned to the left of button
+  });
+
+  $(function() {
+
+    $('.parallax').parallax();
+
+  });
+
+  $(function() {
+
+    $('.scrollspy').scrollSpy();
+    
+  });
+
+
+
+
+})(jQuery);
