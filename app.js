411440e410f2a00852d132da98cf146e710a3d1d
Edward Kemp <edwardlanekemp@gmail.com>
2016-10-03 18:26:21 +0100

    added some back end mailing features, projects section finished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var nodemailer = require('nodemailer');
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -8,7 +9,50 @@ app.use(express.static('app'));
 
 app.use('/', express.static('app/views/'));
 
+app.get('/', function(req, res, next) {
+  var data = {
+    email: req.body.email,
+    subject: req.body.subject,
+    message: req.body.message
+  };
+  res.render('index', {
+    title: 'index'
+  })
+  console.log(data.email)
+})
 
-app.listen(port, function(){
+app.route('/index', function(req, res){
+  res.render('index')
+})
+
+app.post('/', function(req, res, next) {
+  console.log(data.email)
+
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_SECRET,
+      pass: process.env.EMAIL_PASSWORD_SECRET
+    }
+  });
+  var mailOptions = {
+    from: process.env.EMAIL_SECRET,
+    to: 'edwardlanekemp@gmail.com',
+    subject: 'yoyo',
+    message: 'ypyppypypy',
+    html: '<p>This is a test<p>'
+  }
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) {
+      console.log(err);
+      res.redirect('/')
+    } else {
+      console.log('Message Sent ' + info.response)
+    }
+  })
+  console.log('hiii');
+})
+
+app.listen(port, function() {
   console.log('express is listening on ' + port);
 });
