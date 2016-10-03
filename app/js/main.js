455ecdb566f14949bee816e3c7d9cd8ab23f1e49
Edward Kemp <edwardlanekemp@gmail.com>
2016-10-03 10:26:58 +0100

    Made page more responsive added another section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-(function($) {
+$(document).ready(function() {
 
   $('.dropdown-button').dropdown({
     inDuration: 300,
@@ -18,10 +18,49 @@
   $(function() {
 
     $('.scrollspy').scrollSpy();
-    
+
   });
 
+  // var options = [{
+  //   selector: '.title',
+  //   offset: 0,
+  //   callback: function(el) {
+  //     Materialize.fadeInImage($(el));
+  //   }
+  // }, {
+  //   selector: '.description',
+  //   offset: 0,
+  //   callback: function(el) {
+  //     Materialize.fadeInImage($(el));
+  //   }
+  // }, {
+  //   selector: '.timeline-col',
+  //   offset: 0,
+  //   callback: function(el) {
+  //     Materialize.fadeInImage($(el));
+  //   }
+  //
+  // }];
+  // Materialize.scrollFire(options);
 
 
+  $('.collapsible').collapsible({
+    accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
 
-})(jQuery);
+  });
+
+  $('.skillbar').each(function() {
+    $(this).find('.skill-bar').css({
+      width: $(this).attr('data-skills'),
+      backgroundColor: 'rgba(89, 232, 200, 0.95)'
+    })
+    console.log($(this).attr('data-skills'));
+    console.log($(this))
+  })
+
+  //
+  // var $toastContent = $('<span>I am toast content</span>');
+  // Materialize.toast($toastContent, 5000);
+
+
+});
