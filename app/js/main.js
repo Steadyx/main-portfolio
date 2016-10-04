bb33d3e8b260fe6b592fc31e132fc4ed0d46cf5d
Edward Kemp <edwardlanekemp@gmail.com>
2016-10-04 12:52:35 +0100

    Added AJAX for seamless form submission, still working on responsive projects

--- app/js/main.js
+++ app/js/main.js
@@ -58,9 +58,40 @@ $(document).ready(function() {
     console.log($(this))
   })
 
-  //
-  // var $toastContent = $('<span>I am toast content</span>');
-  // Materialize.toast($toastContent, 5000);
+  $(function(handledata) {
+    $('#contact-form').on('submit', function(e) {
+
+      $.ajaxSetup({
+        error: function(jqXHR, exception) {
+          if (jqXHR.status === 0) {
+            console.log('Not connect.\n Verify Network.');
+          } else if (jqXHR.status == 404) {
+            console.log('Requested page not found. [404]');
+          } else if (jqXHR.status == 500) {
+            console.log('Internal Server Error [500].');
+          } else if (exception === 'parsererror') {
+            console.log('Requested JSON parse failed.');
+          } else if (exception === 'timeout') {
+            console.log('Time out error.');
+          } else if (exception === 'abort') {
+            console.log('Ajax request aborted.');
+          } else {
+            console.log('Uncaught Error.\n' + jqXHR.responseText);
+          }
+        }
+      });
 
 
+      $.ajax({
+        type: 'post',
+        url: '/',
+        data: $('#contact-form').serialize(),
+        dataType: 'JSON',
+        success: function(data) {
+          console.log(data.status);
+        }
+      });
+      e.preventDefault();
+    });
+  });
 });
